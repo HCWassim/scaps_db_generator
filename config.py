5b8bcb63025676b66ff97c85c28ba0150ef1aa55
Wassim Hadj-Cherif <hadjcherifwassim@gmail.com>
2026-06-01 07:19:08 +0200

    database generation 5/8

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ CSV_DEF_PATH = os.path.abspath(os.getenv("OUTPUT_CSV_DEF_PATH"))
 SIMULATION_NAME = os.getenv("SIMULATION_FILENAME")
 
 CORE = os.cpu_count() or 4
-BLOC = 3 # varie de 0 à 8
+BLOC = 4 # varie de 0 à 7
 
 # Dopage :
 P0_LABEL1 = "layer 1"
